a7484decf23269336532aaa9b8523c411ff4d7b0
hiphopm <hiphopm@yandex.ru>
2015-05-18 09:18:28 +0500

    Привет от hiphopm

--- ConsoleApplication12/Program.cs
+++ ConsoleApplication12/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication12
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from ehaberev");
+            Console.WriteLine("Hello from hiphopm");
         }
     }
 }
